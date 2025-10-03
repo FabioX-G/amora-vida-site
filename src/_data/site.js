cdcbd12fc0c2452459660493da1a7771c1b3f92d
FabioX-G <57366460+FabioX-G@users.noreply.github.com>
2025-10-03 18:41:23 +0100

    Some update

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,7 @@
 export default {
   name: "Amora Vida",
   tagline: "Inspired by nature, built for life",
+  taglineLines: ["Inspired by nature,", "Built for life"],
   primaryCTA: {
     label: "Request Investor Briefing",
     url: "/contact/"
